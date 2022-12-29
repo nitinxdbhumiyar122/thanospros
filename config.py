91d9e8745d7fd107c833f8f4dc614e60cb535db5
Shiv kumar <121562818+nitinxdbhumiyar122@users.noreply.github.com>
2022-12-29 10:38:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,17 +8,17 @@ class Development(Config):
     # the name to display in your alive message
     ALIVE_NAME = "rishabh"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgres://jrycfssc:fEZMYWiOcO9vB8cIq1_Z97GhW3TiTppJ@tiny.db.elephantsql.com/jrycfssc"
+    DB_URI = "postgres://yjjdrfpn:a5IfsOT5ZxivwDDOXjLDUQQJp6OzRfPN@satao.db.elephantsql.com/yjjdrfpn"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "your session"
+    STRING_SESSION = "1AZWarzkBu2UCDrocccmeR0dGY4EEbSiwdjXyhyFCzZYuLVOPFFGwTf1HfHC8SdNvce-WpIqroOiPwSfXlui_Pu8n9ndh6u8ez94XX2_raKCcAusYPZy6fR0h63eFS1ENrTV0Dt9ZmFTU_A0B90sciBHnhN4npt0z2KFT5GtolwYc8oq2xcZEwnVu_MLqap1UAi_32Bc5FhDHV7cCIVf3htSy-cH4NxDI1m1LmpimhtQdJUaBetYtQtmPX9Hzyk6wxxqHbUDM-ZXGwrhbT58PyH1ATZdYDdpQLeiCchrMtt8tJgyg-1XUCtA3alMo-v2SpdeqIklqPk5lhiKNAL2Ykb_rt_xW8po="
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "your token"
+    TG_BOT_TOKEN = "5842499336:AAHORYhD6U8QBHgi2V5m2w23NFuvS6ISJsY"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID =
+    PRIVATE_GROUP_BOT_API_ID = -1001638711701
     # command handler
     COMMAND_HAND_LER = "."
-    VCMODE = "True"
-    VC_SESSION = "your 2nd id telethon session" #note don't use maim account to vc player
+    VCMODE = "False"
+    #VC_SESSION = "your 2nd id telethon session" #note don't use maim account to vc player
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     # External plugins repo
